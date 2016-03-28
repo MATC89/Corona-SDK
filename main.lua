f9f7caa3566dc3b48c9d0f573cc70beb059230e1
Carla Drieli <carladrieli@gmail.com>
2016-03-28 00:16:29 -0300

    ajustes

--- main.lua
+++ main.lua
@@ -9,37 +9,32 @@ local composer = require ("composer")
 local scene = composer.newScene()
 
 --TextField
-local display = require ("display")
 local native = require ("native")
 txtField = native.newTextField( display.contentCenterX,200, 200, 30 )
 
 -- Fundo
+local display = require ("display")
 local Fundo = display.newImage("fundo1.png", true)
  Fundo.x = display.contentWidth / 2
  Fundo.y = display.contentHeight / 2
 
-
 -- Botão 'ok'
-local widget = require( "widget" )
-
 local function Botao1Evento( event )
-    print( "Teste do Botao" )  
+    
     composer.gotoScene("tela2", "fade", 400 )
 
-    display.remove(group)
     if txtField ~= nil  then
         txtField:removeSelf()
-        txtField = nil
-        print("oieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 1")
+        txtField = nil        
     end
     if botao1 ~= nil then
         botao1:removeSelf()
-        botao1 = nil 
-        print("agoraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 2")   	
+        botao1 = nil         
     end
 
 end
 
+local widget = require( "widget" )
 local botao1 = widget.newButton(
     {
         left = 130,
@@ -53,61 +48,4 @@ local botao1 = widget.newButton(
         
     }
 )
-
---Botao1.strokeColor = { default={ 50, 50, 0 }, over={ 0.4, 0.1, 0.2 } }
-
-local group = display.newGroup()
-group:insert(botao1)
-group:insert(txtField)
-
--- "scene:show()"
-function scene:show( event )
-
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    if ( phase == "will" ) then
-        print("Willlllll")
-    elseif ( phase == "did" ) then
-		print("didddd &&& dd")
-
-    end
-end
-
-
-function scene:create (event)
-    print( "opa! create!" )
-	local sceneGroup = self.view
-end
-
--- "scene:destroy()"
-function scene:destroy( event )
-    print( "opa! destroy!" )
-    local sceneGroup = self.view
-    return true
-    -- Called prior to the removal of scene's view ("sceneGroup").
-    -- Insert code here to clean up the scene.
-    -- Example: remove display objects, save state, etc.
-end
-
-
--- -------------------------------------------------------------------------------
-
--- Listener setup
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "destroy", scene )
-
--- -------------------------------------------------------------------------------
-
 return scene
-
-
-
-
-
-
-
-
-
-
--- tela2.lua
+++ tela2.lua
@@ -3,45 +3,15 @@
 -- Segunda Scene
 --
 ---------------------------
-local display = require ("display")
-
 local composer = require ("composer")
 local scene = composer.newScene()
+local display = require ("display")
 local fundo = display.newImage("fundo2.png", true)
 fundo.x = display.contentWidth / 2
 fundo.y = display.contentHeight / 2
-print ("nova tela")
+
 local myText = display.newText( txtField.text, display.contentCenterX, display.contentCenterY, 200, display.contentHeight * 0.5, native.systemFont, 50)
 myText:setFillColor( 0, 0, 0 )
 
 
-
-function scene:show( event )
-
-	local phase = event.phase
-
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    if ( phase == "will" ) then
-        print("Willlllll tela 2")
-    elseif ( phase == "did" ) then
-		print("didddddd tela2 ")
-
-    end
-end
-
-function scene:create (event)
-	local sceneGroup = self.view
-end
-function scene:destroy( event )
-	print( "destroy main..." )
-	composer.removeScene( "main" )
-	composer.removeScene( "txtField" )
-	composer.removeScene( "botao1" )
-end
-
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "destroy", scene )
 return scene
